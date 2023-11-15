a98ce4dbc277be148d514c42b32e6103de087812
christopheroigo <oigoc60@gmail.com>
2023-11-15 14:51:53 +0300

    free to contribute

---
+++ frontend/views/Tutors/TutorView.tsx
@@ -0,0 +1,7 @@
+export function TutorView(){
+    return (
+        <div>
+            <h1>Tutors</h1>
+        </div>
+    )
+}
---
+++ frontend/views/rooms/RoomsView.tsx
@@ -0,0 +1,7 @@
+export function RoomsView(){
+    return (
+        <div>
+            <h1>Rooms</h1>
+        </div>
+    )
+}
